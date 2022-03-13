a362a62d3bd951a60beb1d1a4ddfe5c60de474e1
NyashaChigura <nyashachigura@gmail.com>
2022-03-13 05:20:36 -0700

    my commit massege

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *my hello, printf
+ *return: ruturn 1 after programm execution
+ */
+int main(void)
+{
+printf("with proper gramma, but the outcome is a piece of art,\n");
+return(0);
+}
